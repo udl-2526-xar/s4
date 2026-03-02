afd64dfc4c854521b8c010e2631e281b961eec6f
Albert Rovira <diphone2020@gmail.com>
2026-03-02 17:52:39 +0100

    initial commit

---
+++ src/SharedUnsafe.java
@@ -0,0 +1,17 @@
+public class SharedUnsafe
+{
+	private static int x = 0;
+
+	public static void main (String[] args) throws InterruptedException
+	{
+
+	}
+
+	private static class Adder implements Runnable
+	{
+		public void run()
+		{
+		}
+	}
+
+}
